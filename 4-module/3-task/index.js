98d25aa8b17522399190319afe2cfbbf3cb469eb
Victoria <vicktorianedopekina@yandex.ru>
2022-06-06 13:35:26 +0400

    dicided 4 module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
-  // ваш код...
+  for (let tr of table.rows) {
+    let age = tr.cells[1].textContent;
+    let gender = tr.cells[2].textContent;
+    let status = tr.cells[3].dataset.available;
+
+    if (status === 'true') {
+      tr.classList.add('available');
+    } else if (status === 'false') {
+      tr.classList.add('unavailable');
+    } else if (!status) {
+      tr.setAttribute('hidden', 'hidden');
+    }
+
+    if (age < 18) {
+      tr.style.textDecoration = 'line-through';
+    }
+
+    let result = (gender === 'm') ? tr.classList.add('male') : tr.classList.add('female');
+  }
 }
